f8b79b0060050851721c68d07b156a1acceb4024
Sergio Morales <chemoral87@hotmail.com>
2026-08-11 10:22:50 -0600

    change port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,16 +1,17 @@
 module.exports = {
-  apps: [{
-    name: 'admin_nuxt4',
-    script: '.output/server/index.mjs',
-    cwd: '/var/www/admin_nuxt4/current',
-    watch: false,
-    instances: 1,
-    autorestart: true,
-    max_restarts: 3,
-    env_file: '.env.production',
-    env: {
-      PORT: 3003,
-      NODE_ENV: 'production'
-    }
-  }]
-}
+  apps: [
+    {
+      name: "admin_nuxt4",
+      script: ".output/server/index.mjs",
+      cwd: "/var/www/admin_nuxt4/current",
+      watch: false,
+      instances: 1,
+      autorestart: true,
+      max_restarts: 3,
+      env: {
+        PORT: 3001,
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
